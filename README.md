40c6e92b0be61eb44a429efbd9bd3cf0ebf7eba8
j23414 <jennifer.chang.bioinform@gmail.com>
2026-08-04 09:11:10 -0500

    Add alluvial plots to compare clusters

--- README.md
+++ README.md
@@ -13,3 +13,15 @@ nextflow run fasta-cluster \
   --segments "WGS" \
   -profile stjude
 ```
+
+## Optional: Alluvial plots to compare various clustering methods
+
+```bash
+# General format of the script (remember to drop the comments, or command does not work)
+Rscript bin/alluvial.R \
+  SequenceName \   #<= One ID to combine cluster files
+  mmseq_cluster.tsv fastani_cluster.tsv mash_cluster.tsv # <= An abitrary number of cluster files, must have a ClusterNumber column
+
+# Generates an alluvial plot in the order that the cluster files are passed in
+open alluvial.png
+```
---
+++ bin/alluvial.R
@@ -0,0 +1,81 @@
+#! /usr/bin/env Rscript
+
+# ===== Argument Parsing ===== #
+# USAGE: alluvial.R one_id_column [an arbitrary number of cluster files]
+# Cluster files should have a ClusterNumber column that will be renamed to the filename
+# [one_id_column]    ClusterNumber
+# ============================ #
+
+args <- commandArgs(trailingOnly = TRUE)
+id_col <- args[1]
+files  <- args[-1]
+
+# == Testing
+# id_col <- "SequenceName"
+# files <- c("blast_results.tsv", "wgs_clusters.tsv", "orf2_clusters.tsv")
+
+# ====== Libraries
+library(tidyverse)
+library(magrittr)
+library(ggalluvial)
+library(purrr)
+
+# ====== Load arbitrary number of cluster files
+tables <- lapply(
+  files,
+  function(f) {
+    x <- read.delim(f, check.names = FALSE)
+    method <- tools::file_path_sans_ext(basename(f))
+    names(x)[names(x) == "ClusterNumber"] <- method
+    x[[method]] <- as.character(x[[method]])
+    x
+  }
+)
+
+# ====== Merge into one data frame
+df <- reduce(
+  tables,
+  inner_join,
+  by = id_col
+)
+
+cluster_cols <- setdiff(names(df), id_col)
+
+plot_df <-
+  df %>%
+  pivot_longer(
+    cols = all_of(cluster_cols),
+    names_to = "method",
+    values_to = "cluster"
+  )
+
+# ====== Plot alluvial
+ggplot(
+  plot_df,
+  aes(
+    x = method,
+    stratum = cluster,
+    alluvium = .data[[id_col]],
+    y = 1,
+    fill = cluster
+  )
+) +
+  geom_flow(alpha = 0.8) +
+  geom_stratum() +
+  geom_text(
+    stat = "stratum",
+    aes(label = after_stat(stratum))
+  ) +
+  theme_classic() +
+  theme(
+    legend.position = "none",
+    axis.title.y = element_blank(),
+    #axis.text.y = element_blank(), # TBD: decide if I want to drop y axis labels later
+    axis.ticks.y = element_blank(),
+    axis.title.x = element_blank(),
+  )
+
+# ==== Save plot
+ggsave("alluvial.png", width=6, height=4)
+
+
